801820431464be3c7073e30eb45bc627a33ba0a4
oz-dahlia-oct <oz.dahlia.oct@gmail.com>
2021-04-24 21:49:46 +0900

    Fundraiser.name is defined

---
+++ test/fundraiser_test.js
@@ -0,0 +1,16 @@
+const FundraiserContract = artifacts.require("Fundraiser");
+
+contract("Fundraiser", accounts => {
+  let fundraiser;
+  const name = "Beneficiary Name";
+
+  describe("initialization", () => {
+    beforeEach (async () => {
+      fundraiser = await FundraiserContract.new(name);
+    });
+    it("gets the Beneficiary name", async () =>{
+      const actual = await fundraiser.name();
+      assert.equal(actual, name, "names should match")
+    });
+  });
+});
